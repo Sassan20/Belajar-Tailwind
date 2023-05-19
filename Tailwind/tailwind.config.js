aff4472b28bce0c7dfb4dcba1e2c1165cb3556f6
Sassan20 <mf80177@gmail.com>
2023-05-19 18:15:02 +0700

    Implementasi Tailwind Ke-4

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     extend: {
       fontFamily : {
         'inter' : ['Inter', 'sans-serif']
+      },
+      animation : {
+        'spin-slow' : 'spin 3s linear infinite',
+        'gempa' : 'gempa 1s linear infinite'
+      },
+      keyframes : {
+        gempa : {
+          '0%, 100%' : {transform : 'rotate(-3deg)'},
+          '50%' : {transform: 'rotate(3deg)'}
+        }
       }
     },
   },
